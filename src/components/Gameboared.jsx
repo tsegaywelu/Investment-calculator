7f056f989c265aabf444e1d8ae40250f122e60e4
Tsegay <tsegaygebrezgi56@gmail.com>
2024-10-16 10:06:54 +0300

    lets to send

--- src/components/Gameboared.jsx
+++ src/components/Gameboared.jsx
@@ -1,30 +1,38 @@
-import React from "react";
-
+import React, { useState } from "react";
 const boared = [
   [null, null, null],
-  [null, "B", "B"],
+  [null, null, null],
   [null, null, null],
 ];
-
 const Gameboared = () => {
+  const [gamebo, setgamebo] = useState(boared);
+  function updateboared(rowindex, colindex) {
+    setgamebo((prev) => {
+      const newarray = [...prev.map((innerarray) => [...innerarray])];
+      newarray[rowindex][colindex] = "X";
+
+      return newarray;
+    });
+  }
   return (
-    <div className="bg-green-800 w-1/2 mx-auto">
-      {boared.map((row, rowIndex) => {
-        return (
-          <div key={rowIndex} className="flex">
-            {" "}
-            {/* Wrapper for each row */}
-            {row.map((col, colIndex) => {
-              return (
-                <button key={colIndex} className="bg-white p-3 m-2">
-                  {col}
+    <ol className="w-1/2 mx-auto bg-black mt-9">
+      {boared.map((row, rowindex) => (
+        <li key={rowindex}>
+          <ol className="flex  justify-between  text-white p-3 ">
+            {row.map((col, colindex) => (
+              <li key={colindex} className=" mx-1 bg-yellow-900 ">
+                <button
+                  className="bg-blue-700 p-8 m-3 "
+                  onClick={() => updateboared(rowindex, colindex)}
+                >
+                  {gamebo[rowindex][colindex]}
                 </button>
-              );
-            })}
-          </div>
-        );
-      })}
-    </div>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
   );
 };
 
